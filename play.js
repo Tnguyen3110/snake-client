f4c4f8cd6da157a815a161043f3e016428afabb4
Tra Nguyen <ntptra85@gmail.com>
2023-04-23 12:29:46 -0600

    initial commit

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-  });
-
-  conn.on("data", () => {
-    console.log("data");
-  });
-
-  return conn;
-};
+const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
